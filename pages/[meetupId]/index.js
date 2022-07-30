86b36407dc8d32401e7d75ec3f4b0351e000f30b
Olugbenga Ajayi <gbejula@gmail.com>
2022-07-30 22:02:30 +0100

    static paths and props

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -11,4 +11,43 @@ function MeetupDetails() {
   );
 }
 
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: 'm1',
+        },
+      },
+      {
+        params: {
+          meetupId: 'm2',
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  // fetch data for meetup
+
+  const meetupId = context.params.meetupId;
+
+  console.log(meetupId);
+
+  return {
+    props: {
+      meetupsData: {
+        image:
+          'https://upload.wikimedia.org/wikipedia/commons/7/7e/Thomas_Wolfe%27s_Home.jpg',
+        id: 'meetupId',
+        title: 'First Meetup',
+        address: 'New stree, new city',
+        description: 'New meetup',
+      },
+    },
+  };
+}
+
 export default MeetupDetails;
